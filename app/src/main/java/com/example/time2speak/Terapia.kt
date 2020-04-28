d4e9ef7fb115f3ff57f3cd2f585aeda4deceaac2
Fernando <exminaruto@gmail.com>
2020-04-28 12:48:38 -0500

    Creacion Activity Terapia

---
+++ app/src/main/java/com/example/time2speak/Terapia.kt
@@ -0,0 +1,12 @@
+package com.example.time2speak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Terapia : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_terapia)
+    }
+}
